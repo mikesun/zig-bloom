ea1d3d0293dbb7b851f4cab492e05dcc56765a69
Mike Sun <mike@msun.io>
2023-08-31 22:34:28 -0400

    WIP.

--- src/main.zig
+++ src/main.zig
@@ -5,17 +5,27 @@ const BloomFilter = @import("bloom_filter.zig").BloomFilter;
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    const num_items: u64 = 100;
+    // Number of items to add to Bloom filter
+    const num_items: u64 = 100_000_000;
     try stdout.print("num_items={}\n", .{num_items});
 
+    // Initialize Bloom filter
     var filter = try BloomFilter.init(std.heap.page_allocator, num_items, 0.01);
     defer filter.deinit();
     std.debug.print("bit_array size in bytes = {}\n", .{filter.bits.bytes.len});
 
-    var buf: [100]u8 = undefined;
+    // Insert items into Bloom filter
     for (0..num_items) |i| {
+        var buf: [100]u8 = undefined;
         const item = try std.fmt.bufPrint(&buf, "item_{d}", .{i});
-        std.debug.print("item: {s}\n", .{item});
         try filter.insert(item);
     }
+
+    // // Verify no false negatives
+    // for (0..num_items) |i| {
+    //     var buf: [100]u8 = undefined;
+    //     const item = try std.fmt.bufPrint(&buf, "item_{d}", .{i});
+    //     if (!try filter.contains(item)) std.debug.panic("false negative: {s}", .{item});
+    // }
+    // try stdout.print("no false negatives\n", .{});
 }
